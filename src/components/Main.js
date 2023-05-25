c50516d11b4eb75a0bc28e8d428d714f7dd39f61
Jaehyeon99 <janhyun99@naver.com>
2023-05-25 19:03:57 +0900

    build 수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
 
 function Main() {
   const settings = {
